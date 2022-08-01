4999de948597403d692a20909d9eeb85a24aec5c
Serg Ryabinin <scanner85@gmail.com>
2022-08-01 08:31:17 +0300

    fix set manufacturer

--- src/main/java/ru/volnamarket/jsonjack/SetManufacturer.java
+++ src/main/java/ru/volnamarket/jsonjack/SetManufacturer.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 import java.sql.BatchUpdateException;
 import java.util.Arrays;
+import java.time.LocalDateTime;
 /**
  *
  * @author vragos
@@ -29,12 +30,59 @@ public class SetManufacturer {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-    
-    public static void main(String args[]) throws SQLException {
-        System.out.println(ANSI_GREEN + "Start set manufacturer_id in oc_product" + ANSI_RESET);
-        Scanner scanner = new Scanner(System.in);
-        String sManufName = scanner.nextLine();
-        upsertManuf(sManufName);
+
+    public static String getManufFromAttr(int iProductId) throws SQLException {
+      String sReturn="";
+      String sSql = "SELECT pa.text FROM oc_product_attribute AS pa WHERE pa.product_id=? "
+        +" AND pa.attribute_id=3";
+      try ( Connection connection = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/volna",
+        "volna", "bBD65855ZLzl@@@###");) 
+      {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // int iManufacturerId = upsertManuf(sManufName);
+        // connection.setAutoCommit(false);
+        PreparedStatement statement = connection.prepareStatement(sSql);
+        statement.setInt(1, iProductId);
+        // System.out.println(statement.toString());
+        ResultSet resultSet = statement.executeQuery();
+        while(resultSet.next()){
+          sReturn = resultSet.getString("text");
+          //System.out.print(ANSI_GREEN + "[" + sReturn + "]" + ANSI_RESET);
+        }
+      } catch (SQLException e) {
+        System.out.println(e.getMessage());
+       } catch (Exception e){
+           e.printStackTrace();
+       }
+      return sReturn;
+    }
+
+    public static void main(String args[]) throws SQLException, IOException {
+        try ( Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/volna",
+                "volna", "bBD65855ZLzl@@@###");) 
+        {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           String sSql = "SELECT product_id FROM oc_product";
+           PreparedStatement statement = connection.prepareStatement(sSql);
+           ResultSet resultSet = statement.executeQuery();
+           while(resultSet.next()){
+              int iProductId = resultSet.getInt("product_id");
+              // System.out.println(iProductId);
+              String sManufName = getManufFromAttr(iProductId);
+              if(!sManufName.equals("")){
+                  int iManufacturerId = upsertManuf(sManufName);
+                  updateProduct(iProductId, iManufacturerId);
+              }
+              // System.out.println("sManufName:" + sManufName);
+              // System.exit(0);
+           }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());  
+        } catch(Exception e) {
+            System.out.println(e.getMessage());  
+        }
     } 
    
     public static boolean ignoreSQLException(String sqlState) {
@@ -83,65 +131,75 @@ public class SetManufacturer {
         }
     }
 
-    public static void upsertManuf(String sManufName) throws SQLException {
-        
+    public static int upsertManuf(String sManufName) throws SQLException {
+        int iManufacturerId = 0;
         try ( Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/volna",
                 "volna", "bBD65855ZLzl@@@###");) 
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //connection.setAutoCommit(false);
             String sSql = "SELECT m.manufacturer_id, m.name, ms.store_id FROM "
                         + "oc_manufacturer AS m LEFT JOIN oc_manufacturer_to_store AS ms "
-                        + "ON m.manufacturer_id=ms.manufacturer_id WHERE m.name= ? ";
+                        + "ON m.manufacturer_id=ms.manufacturer_id WHERE m.name= ?";
             // Statement statement;
-            // statement = connection.createStatement();
             PreparedStatement pStatement = connection.prepareStatement(sSql);
             pStatement.setString(1, sManufName);
-
-            // System.out.println(ANSI_CYAN + pStatement + ANSI_RESET);
-            // System.exit(0);
-
             ResultSet resultSet;
-
-            // sSql = "SELECT * FROM oc_manufacturer";
-            int iManufacturerId;
-            // String sManufName;
             resultSet = pStatement.executeQuery();
-
             if(resultSet != null && resultSet.next()){
-                System.out.println(ANSI_GREEN + "Manufacturer " + sManufName + " exist." 
-                        + ANSI_RESET);
+                iManufacturerId = resultSet.getInt("manufacturer_id");
             } else {
-                System.out.println(ANSI_RED + "Manufacturer " + sManufName + " not exist." 
-                        + ANSI_RESET);
+                String sSqlInsertManuf = "INSERT INTO oc_manufacturer (name, sort_order) " 
+                    +"VALUES ( ?, 1500 )";
+                PreparedStatement pInsertStatement = connection.prepareStatement(
+                    sSqlInsertManuf, Statement.RETURN_GENERATED_KEYS);
+                pInsertStatement.setString(1, sManufName);
+                int iInsertedRows = pInsertStatement.executeUpdate();
+                if ( iInsertedRows == 1){
+                  try (ResultSet keys = pInsertStatement.getGeneratedKeys()) {
+                      if (keys.next()){
+                          iManufacturerId = keys.getInt(1);
+                          String sSqlInsertManufToStore = "INSERT INTO oc_manufacturer_to_store " 
+                            + "(manufacturer_id, store_id) VALUES (?, ?)";
+                          PreparedStatement preparedLast = connection
+                              .prepareStatement(sSqlInsertManufToStore);
+                          preparedLast.setInt(1, iManufacturerId);
+                          preparedLast.setInt(2, 0);
+                          iInsertedRows = preparedLast.executeUpdate();
+                       }
+                  }
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Cause: " + e.getCause());
             System.out.println("SQL state: " + e.getSQLState());
             System.out.println("SQL error code: " + e.getErrorCode());
-            System.out.println("***");
-            // printSQLException(e);
         } catch (Exception ex){
             ex.printStackTrace();
             System.out.println(ex.getMessage());
         } finally {
            
         }
-
-        
+        return iManufacturerId;
     }
     
-    public static void updateProduct(String sSql) throws IOException {
+    public static void updateProduct(int iProductId, int iManufacturerId) throws SQLException {
         try ( Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/volna",
                 "volna", "bBD65855ZLzl@@@###");) 
        {
            Class.forName("com.mysql.cj.jdbc.Driver");
-           connection.setAutoCommit(false);
-           Statement statement = connection.createStatement();
-           statement.executeUpdate(sSql);
+           String sDateTime = "2022-07-31 18:00:00"; 
+           String sSql = "UPDATE oc_product SET manufacturer_id=?, date_modified=? WHERE product_id=?";
+           PreparedStatement statement = connection.prepareStatement(sSql);
+           statement.setInt(1, iManufacturerId);
+           statement.setString(2, sDateTime);
+           statement.setInt(3, iProductId);
+           statement.executeUpdate();
+       } catch (SQLException e) {
+           e.printStackTrace();
+           System.out.println(e.getMessage());
        } catch (Exception e){
            e.printStackTrace();
        }
